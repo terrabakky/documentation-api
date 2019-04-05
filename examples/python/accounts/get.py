19ec9a7a457d0ac8b085cef4dafc528c8f88182f
Tyler Johnson <terrabakky@users.noreply.github.com>
2019-04-05 16:48:57 +1100

    Update get
    Update get.py with python3 syntax and .format methods used on strings. Add example endpoint to make example clearer. Alphabetically order imports.

--- examples/python/accounts/get.py
+++ examples/python/accounts/get.py
@@ -1,10 +1,19 @@
-import requests
-import json
+#!/usr/bin/env python3
+# Get JSON representation of all accounts
 
-#Please substitute YOUR_CC_ACCOUNT_ID, YOUR_ENDPOINT and YOUR_API_KEY
-endpoint = 'YOUR_ENDPOINT'
-accountId = 'YOUR_CC_ACCOUNT_ID'
-url = endpoint + '/v1/accounts/' + accountId
+import json
+import requests
+
+# Please substitute YOUR_CC_ACCOUNT_ID and YOUR_API_KEY
+# Change the endpoint to use the correct one for your acconut
+
+# Substitute the endpoint below for the correct one for your account
+BASE = 'https://ap-southeast-2-api.cloudconformity.com/v1/{endpoint}'
+account_id = 'YOUR_CC_ACCOUNT_ID'
+
+endpoint = 'accounts/{account_id}'.format(account_id=account_id)
+url = BASE.format(endpoint=endpoint)
+
 
 headers = {
 	'Content-Type': 'application/vnd.api+json',
@@ -13,4 +22,4 @@ headers = {
 
 resp = requests.get(url, headers=headers)
 
-print json.dumps(resp.json(), indent=4, sort_keys=True)
+print(json.dumps(resp.json(), indent=4, sort_keys=True))
